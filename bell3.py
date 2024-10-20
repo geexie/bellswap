525057eba6a22c6594e880f553c62b3f031cd999
geexie <marina@u.nus.edu>
2024-10-20 20:07:22 +0800

    completes HBell-basis & RxBell-basis versions

--- bell3.py
+++ bell3.py
@@ -7,61 +7,64 @@ from qiskit.primitives import Sampler
 from qiskit.visualization import plot_histogram
 from qiskit.quantum_info import Statevector
 from qiskit.visualization import plot_state_qsphere
+from numpy import sqrt, pi
 
 # Create a Bell state quantum circuit
 cr=ClassicalRegister(2,'c')
 bell = QuantumCircuit(QuantumRegister(1, 'A'), QuantumRegister(1, 'B'), QuantumRegister(1, 'C'), QuantumRegister(1, 'D'), cr)
 
-# Z -> HBell
-bell.h(0)
+bell.rx(pi/3,0)
+bell.s(0)
+bell.x(0)
 bell.cx(0, 1)
-bell.h(0)
 
-# for \Psi^+
+# for \Psi+
+bell.x(3)
+
+bell.rx(pi/3,2)
+bell.s(2)
 bell.x(2)
-
-# Z -> HBell
-bell.h(2)
 bell.cx(2, 3)
-bell.h(2)
 
-full = True# False #True
+full = False# False #True
 if (full == True):
     bell.barrier()
 
-    # ~Bell measurement of 1 and 2
-    # HBell -> Z
-    bell.h(1)
-    bell.cx(1,2)
-    bell.h(1)
+    # Bell measurement of 1 and 2
+    bell.cx(1, 2)
+    bell.x(1)
+    bell.z(1)
+    bell.s(1)
+    bell.rx(-pi/3,1)
     bell.barrier()
-    # bell.measure([1,2],[0,1])
 
-    # deferred measurement principal
     # recover original bell state
     bell.cx(2, 0)
+    # bell.crx(pi/3,2,0)
     bell.cz(1, 0)
     bell.barrier()
 
-    # Z -> HBell (0,3)
-    bell.h(0)
-    bell.cx(0,3)
-    bell.h(0)
+    # reverce 0,3 to computational basis
+    bell.cx(0, 3)
+    bell.x(0)
+    bell.z(0)
+    bell.s(0)
+    bell.rx(-pi/3,0)
     bell.barrier()
 
-evolve = True #False# True
+evolve = True #False# 
 if (evolve == True):
     bell = bell.reverse_bits()
     sv_prepare = Statevector([1,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0])
     new_sv_prepare = sv_prepare.evolve(bell)
     print(new_sv_prepare)
-    plot_state_qsphere(new_sv_prepare, filename = "sv_bell2_full.png" if (full == True) else "sv_bell2_init.png")
+    plot_state_qsphere(new_sv_prepare, filename = "sv_bell3_full.png" if (full == True) else "sv_bell3_init.png")
 else:
     bell.measure([0, 3],[0, 1])
     bell = bell.reverse_bits()
     print("Original Circuit:")
     print(bell)
-    bell.draw("mpl", filename="c_bell2.png")
+    bell.draw("mpl", filename="c_bell3.png")
 
     # Use the Sampler to simulate the circuit
     sampler = Sampler()
@@ -75,4 +78,4 @@ else:
     print("Measuremet result:", result.quasi_dists[0])
 
     # Plot the result as a histogram
-    plot_histogram(counts, filename="h_bell2.png")
+    plot_histogram(counts, filename="h_bell3.png")
